4716e4b31b9af27d33956aa3915e891749c9c3b6
Iván Villa Sánchez <ivan.villa.sanchez@outlook.com>
2016-11-04 17:38:25 +0100

    - Probando cargar la lista de Hosts del Json

--- app/src/main/java/com/example/klaussius/marvin_mobile/MainMenu.java
+++ app/src/main/java/com/example/klaussius/marvin_mobile/MainMenu.java
@@ -1,13 +1,30 @@
 package com.example.klaussius.marvin_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
+
+    Button btHostTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        btHostTest = (Button)findViewById(R.id.btHostTest);
+        btHostTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchHosts();
+            }
+        });
+    }
+
+    public void launchHosts(){
+        Intent myIntent = new Intent(this,TestHost.class);
+        startActivity(myIntent);
     }
 }
